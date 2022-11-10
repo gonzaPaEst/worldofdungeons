713d782ebe5ac9a9716cad3987e62541ddc190fc
Gonzalo Paez <gonzapaez@gmail.com>
2022-11-10 18:08:22 -0500

    pass sheet as object to sheetConfig; hide TOML completely

--- modules/worldofdungeons.js
+++ modules/worldofdungeons.js
@@ -1,144 +1,19 @@
 Hooks.once("ready", async function () {
 
-  setTimeout(() => {
-    $("div#pbta-sheet-config button[type='submit']").click();
-  }, 2000)
+  let newSheet = { rollFormula: "2d6", rollResults: { failure: { start: null, end: 6, label: "Miss" }, partial: { start: 7, end: 9, label: "Partial Success" }, success: { start: 10, end: 11, label: "Full Success!" }, critical: { start: 12, end: null, label: "Critical Success!" } }, actorTypes: { character: { stats: { str: { label: "STR", value: 0 }, int: { label: "INT", value: 0 }, con: { label: "CON", value: 0 }, dex: { label: "DEX", value: 0 }, wis: { label: "WIS", value: 0 }, cha: { label: "CHA", value: 0 } }, attrTop: { armor: { label: "Total Armor", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 }, hp: { label: "Hit Points", description: null, customLabel: false, userLabel: false, type: "Resource", value: 0, max: 0 }, hitdice: { label: "Hit Dice", description: null, customLabel: false, userLabel: false, type: "Roll", value: "d6" }, coin: { label: "Coin", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 } }, attrLeft: { armorspeed: { label: "Armor / Speed", description: null, customLabel: false, userLabel: false, type: "ListMany", condition: false, options: { 0: { label: "None / Fast (0 armor)", value: false }, 1: { label: "Light / Normal (1 armor)", value: false }, 2: { label: "Full / Slow (2 armor)", value: false }, 3: { label: "Shield (+1 armor)", value: false } } }, level: { label: "Level", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 }, xp: { label: "XP", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 }, nextlevel: { label: "Next Level", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 } }, moveTypes: { skills: { label: "Skills", moves: [] }, special: { label: "Special Abilities", moves: [] } }, equipmentTypes: { weapons: { label: "Weapons", moves: [] }, equipment: { label: "Equipment", moves: [] } } }, npc: { attrTop: { hp: { label: "HP", description: null, customLabel: false, userLabel: false, type: "Resource", value: 0, max: 0 }, armor: { label: "Armor", description: null, customLabel: false, userLabel: false, type: "Number", value: 0 }, damage: { label: "Damage", description: null, customLabel: false, userLabel: false, type: "Roll", value: "d6" } }, attrLeft: { instinct: { label: "Intinct", description: null, customLabel: false, userLabel: false, type: "LongText", value: "" } }, moveTypes: { gm: { label: "GM Moves", moves: [] } } } } }
 
-  setTimeout(() => {
-    $("div#pbta-sheet-config a.header-button.close").click();
-  }, 2500)
-
-  let newSheet = `
-# Configure Rolls
-rollFormula = "2d6"
-  
-# Define roll result ranges.
-[rollResults]
-  [rollResults.failure]
-    range = "6-"
-    label = "Miss"
-  [rollResults.partial]
-    range = "7-9"
-    label = "Partial Success"
-  [rollResults.success]
-    range = "10-11"
-    label = "Full Success!"
-  [rollResults.critical]
-    range = "12+"
-    label = "Critical Success!"
-
-########################################
-## CHARACTERS ##########################
-########################################
-# Define the character group.
-[character]
-
-  # Define stats.
-  [character.stats]
-    str = "STR"
-    int = "INT"
-    con = "CON"
-    dex = "DEX"
-    wis = "WIS"
-    cha = "CHA"
-
-  # Define attributes.
-  [character.attributesTop]
-    [character.attributesTop.armor]
-      type = "Number"
-      label = "Total Armor"
-    [character.attributesTop.hp]
-      type = "Resource"
-      label = "Hit Points"
-    [character.attributesTop.hitdice]
-      type = "Roll"
-      label = "Hit Dice"
-      default = "d6"
-    [character.attributesTop.coin]
-      type = "Number"
-      label = "Coin"
-
-  # Define sidebar details.
-  [character.attributesLeft]
-    [character.attributesLeft.armorspeed]
-      type = "ListMany"
-      label = "Armor & Speed"
-      condition = false
-      options = [
-        "None & Fast (0 armor)",
-        "Light & Normal (1 armor)",
-        "Full & Slow (2 armor)",
-        "Shield (+1 armor)"
-      ]
-      
-    [character.attributesLeft.level]
-      type = "Number"
-      label = "Level"
-      
-    [character.attributesLeft.xp]
-      type = "Number"
-      label = "XP"
-      
-    [character.attributesLeft.nextlevel]
-      type = "Number"
-      label = "Needed for Next Level"
-
-  # Define groups for moves.
-  [character.moveTypes]
-    skills = "Skills"
-    special = "Special Abilities"
-
-  # Define groups for equipment.
-  [character.equipmentTypes]
-    weapons = "Weapons"
-    equipment = "Equipment"
-
-########################################
-## NPCS ################################
-########################################
-# Define stats.
-[npc]
-  # Define attributes.
-  [npc.attributesTop]
-    [npc.attributesTop.hp]
-      type = "Resource"
-      label = "HP"
-    [npc.attributesTop.armor]
-      type = "Number"
-      label = "Armor"
-    [npc.attributesTop.damage]
-      type = "Roll"
-      label = "Damage"
-      default = "d6"
-      
-  [npc.attributesLeft]
-    [npc.attributesLeft.traits]
-      type = "LongText"
-      label = "Traits"
-    [npc.attributesLeft.instinct]
-      type = "LongText"
-      label = "Intinct"
-      
-    [npc.attributesLeft.knacks]
-      type = "LongText"
-      label = "Knacks"
-
-  # Define logical groups for moves.
-  [npc.moveTypes]
-    gm = "GM Moves"
-`;
-
-  await game.settings.set('pbta', 'advForward', true);
-  await game.settings.set('pbta', 'hideRollFormula', true);
-  await game.settings.set('pbta', 'hideForward', true);
-  await game.settings.set('pbta', 'hideOngoing', true);
-  await game.settings.set('pbta', 'hideRollMode', true);
-  await game.settings.set('pbta', 'hideUses', true);
-
-  const data = await game.settings.get("pbta", "sheetConfig");
-  if (!data.tomlString) {
-    data.tomlString = newSheet;
-    await game.settings.set("pbta", "sheetConfig", data);
-  };
+  let data = await game.settings.get("pbta", "sheetConfig");
+  if(data == '') {
+    await game.settings.set('pbta', 'advForward', true);
+    await game.settings.set('pbta', 'hideRollFormula', true);
+    await game.settings.set('pbta', 'hideForward', true);
+    await game.settings.set('pbta', 'hideOngoing', true);
+    await game.settings.set('pbta', 'hideRollMode', true);
+    await game.settings.set('pbta', 'hideUses', true);
+    await game.settings.set("pbta", "sheetConfig", newSheet);
+    setTimeout(() => {
+      location.reload();
+    }, 1000)
+  }
 
 });
